581a496126875a02f2da91944ece5b5f341c35d4
Jonathan Sick <jsick@lsst.org>
2021-05-04 16:44:16 -0400

    Update conf.py for documenteer 0.6

--- doc/conf.py
+++ doc/conf.py
@@ -1,13 +1,13 @@
 """Sphinx configuration file for an LSST stack package.
-
-This configuration only affects single-package Sphinx documenation builds.
+This configuration only affects single-package Sphinx documentation builds.
+For more information, see:
+https://developer.lsst.io/stack/building-single-package-docs.html
 """
 
-from documenteer.sphinxconfig.stackconf import build_package_configs
-import lsst.cbp
+from documenteer.conf.pipelinespkg import *
 
 
-_g = globals()
-_g.update(build_package_configs(
-    project_name='cbp',
-    version=lsst.cbp.version.__version__))
+project = "cbp"
+html_theme_options["logotext"] = project
+html_title = project
+html_short_title = project
